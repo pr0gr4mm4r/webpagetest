ec9fc038eff6c830a045687ab50ad83da299b809
pr0gr4mm4r <david@macbookair.local>
2024-05-20 22:42:23 +0200

    new style layer

--- src/components/personal.js
+++ src/components/personal.js
@@ -4,16 +4,17 @@ import logo from './webseitenportrait.jpeg'
 export default function Personal() {
   return (
     <div>
-      <div className="row">
+      <div className="row" style={{ marginTop: '20px' }}>
         <div className="col-md-4">
-          <img src={logo} className="img-fluid" alt="Portrait von David" />
+          <img src={logo} className="img-fluid" alt="Portrait von David" style={{ marginTop: '20px' }} />
         </div>
-        <div className="col-md-8" >
+        <div className="col-md-8" style={{ marginTop: '20px' }}>
           <h2>Über mich</h2>
-          <p>Hallo, mein Name ist David und ich bin von Beruf Systemadministrator. Neben meiner regulären Tätigkeit als Systemadministrator biete ich meine Dienstleistungen auch als Freelancer an. Ich habe Erfahrung in verschiedenen Bereichen der IT-Infrastruktur und -Administration.</p>
+          <p>Hallo, mein Name ist David und ich bin von Beruf Systemadministrator. Ich habe meinen Bachelor in "Angewandter Informatik" an der DHBW Karlsruhe gemacht. Neben meiner regulären Arbeit im BLM Karlsruhe biete ich meine Dienstleistungen auch als Freelancer an. Ich habe Erfahrung in verschiedenen Bereichen der IT-Infrastruktur und -Administration.</p>
           <p>Einige meiner bisherigen Kunden sind Manimedia und Korrektur.de, für die ich erfolgreich Projekte umgesetzt und IT-Lösungen bereitgestellt habe. Ich bin bestrebt, meinen Kunden stets die bestmöglichen Lösungen anzubieten und sie bei der Optimierung ihrer Systeme zu unterstützen.</p>
           <p>Meine Leidenschaft für Technologie und mein Engagement für eine zuverlässige und effiziente IT-Infrastruktur treiben mich an, kontinuierlich mein Wissen zu erweitern und mich neuen Herausforderungen zu stellen.</p>
           <p>Wenn Sie Interesse an meinen Dienstleistungen haben oder Fragen haben, zögern Sie nicht, mich zu kontaktieren. Ich freue mich darauf, mit Ihnen zusammenzuarbeiten!</p>
+          <p>    - david.baur@manimedia.de -</p>
         </div>
       </div>
     </div>
