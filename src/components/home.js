cc5190d265e573ac015ab2d44ff44555251684bf
pr0gr4mm4r <david@macbookair.local>
2024-05-25 15:40:01 +0200

    new style layer

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import backgroundImageUrl from './nature-8727795.jpg';
 
 const Home = () => {
+
   return (
     <div style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-// Erlaube vertikales Scrollen, wenn der Inhalt größer als der Bildschirm ist
       backgroundImage: `url(${backgroundImageUrl})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      minHeight: `1000px`, 
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
@@ -46,16 +43,34 @@ const Home = () => {
       </section>
 
       <section style={{ marginBottom: '2em', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '1em', borderRadius: '10px' }}>
-        <h2>About me</h2>
+        <h2>About us</h2>
         <p>
-          Hier kannst du eine kurze Beschreibung über dein Unternehmen und dessen Geschichte hinzufügen.
+          We are a humble IT company located in Kalrsruhe.
         </p>
+        <button style={{
+            padding: '0.5em 2em',
+            fontSize: '1em',
+            color: '#fff',
+            backgroundColor: '#007BFF',
+            border: 'none',
+            borderRadius: '0.3em',
+            cursor: 'pointer',
+          }}>Experience more</button>
       </section>
 
       <section style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '1em', borderRadius: '10px' }}>
-        <h2>Our Service Portfolio</h2>
+        <h2>Unsere Leistungen</h2>
         <p>
-          Hier kannst du eine detaillierte Beschreibung deiner Leistungen hinzufügen.
+          extending IT-Infrastructure (private or business)
+        </p>
+        <p>
+          developing and installation of software solutions
+        </p>
+        <p>
+          data transfer
+        </p>
+        <p>
+          consulting
         </p>
       </section>
     </div>
